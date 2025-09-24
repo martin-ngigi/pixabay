9cd05a6a3008688c924ae6fd384c66c7839dcc42
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-09-24 14:11:39 +0300

    Added Side menu

--- lib/features/dashboard/views/dashboard_view.dart
+++ lib/features/dashboard/views/dashboard_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pixabay/features/dashboard/views/side_menu.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -10,12 +11,55 @@ class DashboardView extends StatefulWidget {
 class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text("Dash board view")
-        ],
-      ),
+    return LayoutBuilder(
+        builder: (context, constraints){
+          bool isLargeScreen = constraints.maxWidth >= 800;
+
+          // Show permanent side menu for large screens
+          if (isLargeScreen) {
+            return Scaffold(
+              body: Row(
+                children: [
+
+                  const SizedBox(
+                    width: 250,
+                    child: SideMenuView(),
+                  ),
+
+                  const VerticalDivider(width: 1),
+
+                  Expanded(
+                      child: Center(
+                        child: Text(
+                            "Main content",
+                          style: Theme.of(context).textTheme.headlineLarge,
+                        ),
+                      ),
+
+
+                  )
+                ],
+              ),
+            );
+          }
+          else { // Show Drawer (hamburger menu)
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Main content"),
+              ),
+              drawer: const Drawer(
+                child: SideMenuView(),
+              ),
+              body: Center(
+                child: Text(
+                  "Main Content",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+            );
+          }
+
+        }
     );
   }
 }
